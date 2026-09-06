efa44c6679f78aa04699aaf22ec05fcaa25c7052
apontarelli <antoniopontarelli@gmail.com>
2026-09-06 10:25:01 -0700

    Serialize process-heavy Git integration tests in CI

--- elixir/test/symphony_elixir/operator_mutation_test.exs
+++ elixir/test/symphony_elixir/operator_mutation_test.exs
@@ -1,5 +1,5 @@
 defmodule SymphonyElixir.OperatorMutationTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   alias SymphonyElixir.{ControlPlane, ExecutionContext, HostScheduler, OperatorInterface, OperatorMutation, PathSafety}
   alias SymphonyElixir.HostScheduler.Registry
